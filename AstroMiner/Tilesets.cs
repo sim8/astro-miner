cd305a86611f93e6f90221e1f32b1c1bfb5a96aa
Sim8 <simeon.k.lees@gmail.com>
2024-09-17 23:10:16 +0100

    Improve perf

--- AstroMiner/Tilesets.cs
+++ AstroMiner/Tilesets.cs
@@ -1,40 +1,43 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace AstroMiner;
 
+[Flags]
 internal enum Neighbours
 {
-    Above,
-    AboveRight,
-    Right,
-    BelowRight,
-    Below,
-    BelowLeft,
-    Left,
-    AboveLeft
+    None = 0,
+    Above = 1 << 0, // 1
+    AboveRight = 1 << 1, // 2
+    Right = 1 << 2, // 4
+    BelowRight = 1 << 3, // 8
+    Below = 1 << 4, // 16
+    BelowLeft = 1 << 5, // 32
+    Left = 1 << 6, // 64
+    AboveLeft = 1 << 7 // 128
 }
 
 public static class Tilesets
 {
-    private static readonly Dictionary<Neighbours, (int, int)> Offsets;
-    private static readonly Neighbours[] AllNeighbours;
+    private static readonly (int, int)[] Offsets;
     public static readonly Dictionary<CellState, string> TilesetTextureNames;
 
     static Tilesets()
     {
-        Offsets = new Dictionary<Neighbours, (int, int)>
+        // Initialize the offsets for each neighbour, indexed by bit position (0-7)
+        Offsets = new (int, int)[8]
         {
-            { Neighbours.Above, (0, -1) },
-            { Neighbours.AboveRight, (1, -1) },
-            { Neighbours.Right, (1, 0) },
-            { Neighbours.BelowRight, (1, 1) },
-            { Neighbours.Below, (0, 1) },
-            { Neighbours.BelowLeft, (-1, 1) },
-            { Neighbours.Left, (-1, 0) },
-            { Neighbours.AboveLeft, (-1, -1) }
+            (0, -1), // Above (bit position 0)
+            (1, -1), // AboveRight (bit position 1)
+            (1, 0), // Right (bit position 2)
+            (1, 1), // BelowRight (bit position 3)
+            (0, 1), // Below (bit position 4)
+            (-1, 1), // BelowLeft (bit position 5)
+            (-1, 0), // Left (bit position 6)
+            (-1, -1) // AboveLeft (bit position 7)
         };
+
         TilesetTextureNames = new Dictionary<CellState, string>
         {
             { CellState.Rock, "rock-tileset" },
@@ -42,16 +45,15 @@ public static class Tilesets
             { CellState.Diamond, "diamond-tileset" },
             { CellState.Ruby, "ruby-tileset" }
         };
-        AllNeighbours = (Neighbours[])Enum.GetValues(typeof(Neighbours));
     }
-
 
     public static (int, int) GetTileCoords(MiningState state, int col, int row)
     {
         // Dark
-        if (AllFilledExcept(state, col, row)) return (1, 2);
+        if (AllFilledExcept(state, col, row))
+            return (1, 2);
 
-        // Concave
+        // Concave corners
         if (AllFilledExcept(state, col, row, Neighbours.AboveRight))
             return (2, 2);
         if (AllFilledExcept(state, col, row, Neighbours.BelowRight))
@@ -61,70 +63,70 @@ public static class Tilesets
         if (AllFilledExcept(state, col, row, Neighbours.AboveLeft))
             return (4, 2);
 
-        // Double concave
-        if (AllFilledExcept(state, col, row, Neighbours.AboveRight, Neighbours.BelowLeft))
+        // Double concave corners
+        if (AllFilledExcept(state, col, row, Neighbours.AboveRight | Neighbours.BelowLeft))
             return (5, 0);
-        if (AllFilledExcept(state, col, row, Neighbours.AboveLeft, Neighbours.BelowRight))
+        if (AllFilledExcept(state, col, row, Neighbours.AboveLeft | Neighbours.BelowRight))
             return (5, 1);
 
         // Flat edges
-        if (AllFilledExcept([Neighbours.AboveLeft, Neighbours.Above, Neighbours.AboveRight], state, col, row))
+        if (AllFilledExcept(state, col, row, Neighbours.None,
+                Neighbours.AboveLeft | Neighbours.Above | Neighbours.AboveRight))
             return (3, 2);
-        if (AllFilledExcept([Neighbours.AboveRight, Neighbours.Right, Neighbours.BelowRight], state, col, row))
+        if (AllFilledExcept(state, col, row, Neighbours.None,
+                Neighbours.AboveRight | Neighbours.Right | Neighbours.BelowRight))
             return (2, 1);
-        if (AllFilledExcept([Neighbours.BelowLeft, Neighbours.Below, Neighbours.BelowRight], state, col, row))
+        if (AllFilledExcept(state, col, row, Neighbours.None,
+                Neighbours.BelowLeft | Neighbours.Below | Neighbours.BelowRight))
             return (3, 0);
-        if (AllFilledExcept([Neighbours.AboveLeft, Neighbours.Left, Neighbours.BelowLeft], state, col, row))
+        if (AllFilledExcept(state, col, row, Neighbours.None,
+                Neighbours.AboveLeft | Neighbours.Left | Neighbours.BelowLeft))
             return (4, 1);
 
-        // Convex
-        if (AllFilledExcept(
-            [
-                Neighbours.AboveLeft, Neighbours.Above, Neighbours.AboveRight, Neighbours.Right, Neighbours.BelowRight
-            ], state, col, row))
+        // Convex corners
+        if (AllFilledExcept(state, col, row, Neighbours.None,
+                Neighbours.AboveLeft | Neighbours.Above | Neighbours.AboveRight | Neighbours.Right |
+                Neighbours.BelowRight))
             return (1, 0);
-        if (AllFilledExcept(
-                [
-                    Neighbours.AboveRight, Neighbours.Right, Neighbours.BelowRight, Neighbours.Below,
-                    Neighbours.BelowLeft
-                ], state, col, row,
-                Neighbours.Right, Neighbours.Below))
+        if (AllFilledExcept(state, col, row, Neighbours.Right | Neighbours.Below,
+                Neighbours.AboveRight | Neighbours.Right | Neighbours.BelowRight | Neighbours.Below |
+                Neighbours.BelowLeft))
             return (1, 1);
-        if (AllFilledExcept(
-                [Neighbours.BelowRight, Neighbours.Below, Neighbours.BelowLeft, Neighbours.Left, Neighbours.AboveLeft],
-                state, col, row,
-                Neighbours.Below, Neighbours.Left))
+        if (AllFilledExcept(state, col, row, Neighbours.Below | Neighbours.Left,
+                Neighbours.BelowRight | Neighbours.Below | Neighbours.BelowLeft | Neighbours.Left |
+                Neighbours.AboveLeft))
             return (0, 1);
-        if (AllFilledExcept(
-                [Neighbours.BelowLeft, Neighbours.Left, Neighbours.AboveLeft, Neighbours.Above, Neighbours.AboveRight],
-                state, col, row,
-                Neighbours.Left, Neighbours.Above))
+        if (AllFilledExcept(state, col, row, Neighbours.Left | Neighbours.Above,
+                Neighbours.BelowLeft | Neighbours.Left | Neighbours.AboveLeft | Neighbours.Above |
+                Neighbours.AboveRight))
             return (0, 0);
 
         // Island
         return (0, 2);
     }
 
-
-    private static bool AllFilledExcept(MiningState state, int col, int row,
-        params Neighbours[] shouldBeEmptyNeighbours)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static bool AllFilledExcept(
+        MiningState state,
+        int col,
+        int row,
+        Neighbours shouldBeEmptyNeighbours = Neighbours.None,
+        Neighbours disregardNeighbours = Neighbours.None)
     {
-        return AllFilledExcept([], state, col, row, shouldBeEmptyNeighbours);
-    }
-
-    private static bool AllFilledExcept(Neighbours[] disregardNeighbours, MiningState state, int col, int row,
-        params Neighbours[] shouldBeEmptyNeighbours)
-    {
-        foreach (var neighbour in AllNeighbours)
+        for (var i = 0; i < 8; i++)
         {
-            if (disregardNeighbours.Contains(neighbour)) continue;
+            var neighbour = (Neighbours)(1 << i);
 
-            var offset = Offsets[neighbour];
+            if ((disregardNeighbours & neighbour) != Neighbours.None)
+                continue;
+
+            var offset = Offsets[i];
             var cellState = state.GetCellState(col + offset.Item1, row + offset.Item2);
             var isFilled = cellState != CellState.Empty && cellState != CellState.Floor;
-            var shouldBeFilled = !shouldBeEmptyNeighbours.Contains(neighbour);
+            var shouldBeFilled = (shouldBeEmptyNeighbours & neighbour) == Neighbours.None;
 
-            if (isFilled != shouldBeFilled) return false;
+            if (isFilled != shouldBeFilled)
+                return false;
         }
 
         return true;
