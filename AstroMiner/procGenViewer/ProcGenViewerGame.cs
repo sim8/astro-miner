32738f1542612a9ff5321cfd52f4ef6d21b61b7f
Simeon Lees <simeon.k.lees@gmail.com>
2024-12-07 20:14:39 +0000

    Dedicated textures for proc gen reviewing

--- AstroMiner/procGenViewer/ProcGenViewerGame.cs
+++ AstroMiner/procGenViewer/ProcGenViewerGame.cs
@@ -35,27 +35,16 @@ public class ProcGenViewerGame : Game
 
     private void LoadTexture(string name)
     {
-        _textures[name] = Content.Load<Texture2D>($"img/{name}");
+        _textures[name] = Content.Load<Texture2D>($"img-proc-gen/{name}");
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        LoadTexture("dogica-font");
-        LoadTexture("floor");
         LoadTexture("rock-tileset");
         LoadTexture("solid-rock-tileset");
         LoadTexture("ruby-tileset");
         LoadTexture("diamond-tileset");
-        LoadTexture("player");
-        LoadTexture("miner-no-tracks");
-        LoadTexture("tracks-0");
-        LoadTexture("tracks-1");
-        LoadTexture("tracks-2");
-        LoadTexture("dark-screen");
-        LoadTexture("radial-light");
-        LoadTexture("directional-light");
-        LoadTexture("dynamite");
     }
 
     protected override void Update(GameTime gameTime)
