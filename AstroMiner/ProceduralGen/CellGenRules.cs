00b17d5116ee741b22bfc26ea50a875b188b269b
Simeon Lees <simeon.k.lees@gmail.com>
2025-01-05 22:45:29 +0000

    some improvements

--- AstroMiner/ProceduralGen/CellGenRules.cs
+++ AstroMiner/ProceduralGen/CellGenRules.cs
@@ -1,5 +1,4 @@
 #nullable enable
-using System;
 using System.Collections.Generic;
 using AstroMiner.Definitions;
 
@@ -7,41 +6,73 @@ namespace AstroMiner.ProceduralGen;
 
 public static class CellGenRules
 {
+    // TODO consts for "borders" between two cell types
     private static readonly List<Rule> _orderedRules = new()
     {
-        // Lava perimeter
+        //----------------------------------------------
+        // CORE RULES
+        //----------------------------------------------
+        new Rule(new RuleParams
+        {
+            CellType = CellType.Diamond,
+            EndDistance = GameConfig.AsteroidGen.CoreRadius,
+            Noise1Range = (0.7f, 1f)
+        }),
+        new Rule(new RuleParams
+        {
+            CellType = CellType.SolidRock,
+            EndDistance = GameConfig.AsteroidGen.CoreRadius,
+            Noise1Range = (0.56f, 1f)
+        }),
+        new Rule(new RuleParams
+        {
+            CellType = CellType.ExplosiveRock,
+            EndDistance = GameConfig.AsteroidGen.CoreRadius,
+            Noise1Range = (0.38f, 0.39f)
+        }),
+        //----------------------------------------------
+        // MANTLE RULES
+        //----------------------------------------------
         new Rule(new RuleParams
         {
             CellType = CellType.Floor,
             StartDistance = GameConfig.AsteroidGen.CoreRadius,
             EndDistance = GameConfig.AsteroidGen.MantleRadius,
-            Noise1Target = 0.3f,
-            Noise2Target = 0.485f,
-            BaseAllowance = 0.3f,
-            AllowanceModifier = (allowance, distanceFromStartOfRuleLayerPercentage) =>
-                allowance * distanceFromStartOfRuleLayerPercentage
+            Noise1Range = (0.4f, 0.6f),
+            Noise2Range = (0.42f, 0.55f)
         }),
         new Rule(new RuleParams
         {
             CellType = CellType.Lava,
             StartDistance = GameConfig.AsteroidGen.CoreRadius,
             EndDistance = GameConfig.AsteroidGen.MantleRadius,
-            Noise2Target = 0.775f,
-            BaseAllowance = 0.225f
+            Noise2Range = (0.55f, 1f)
+        }),
+        new Rule(new RuleParams
+        {
+            CellType = CellType.SolidRock,
+            StartDistance = GameConfig.AsteroidGen.CoreRadius,
+            EndDistance = GameConfig.AsteroidGen.MantleRadius,
+            Noise1Range = (0.65f, 1f)
         }),
         new Rule(new RuleParams
         {
             CellType = CellType.Gold,
             StartDistance = GameConfig.AsteroidGen.CoreRadius - 0.1f,
             EndDistance = GameConfig.AsteroidGen.CoreRadius + 0.2f,
-            Noise2Target = 0.77f,
-            BaseAllowance = 0.04f
+            Noise1Range = (0.42f, 0.43f),
+            Noise2Range = (0.42f, 0.55f)
         }),
+        //----------------------------------------------
+        // CRUST RULES
+        //----------------------------------------------
+        //----------------------------------------------
+        // ALL LAYERS RULES
+        //----------------------------------------------
         new Rule(new RuleParams
         {
             CellType = CellType.Ruby,
-            Noise1Target = 0.4125f,
-            BaseAllowance = 0.0025f
+            Noise1Range = (0.410f, 0.415f)
         }),
         new Rule(new RuleParams
         {
@@ -63,10 +94,10 @@ public static class CellGenRules
         public CellType CellType { get; set; }
         public float? StartDistance { get; set; }
         public float? EndDistance { get; set; }
-        public float? Noise1Target { get; set; }
-        public float? Noise2Target { get; set; }
-        public float BaseAllowance { get; set; }
-        public Func<float, float, float>? AllowanceModifier { get; set; }
+        public (float start, float end)? Noise1Range { get; set; }
+
+        public (float start, float end)? Noise2Range { get; set; }
+        // public Func<float, float, bool>? CustomRangeCheck { get; set; }
     }
 
     public class Rule(
@@ -82,26 +113,18 @@ public static class CellGenRules
             // Early return if distance out of bounds of current rule
             if (distancePerc < startDistance || distancePerc > endDistance) return false;
 
-            var allowance = p.BaseAllowance;
-            if (p.AllowanceModifier != null)
-            {
-                var layerSize = endDistance - startDistance;
-                var distanceFromStartOfRuleLayer = distancePerc - startDistance;
-                var distanceFromStartOfRuleLayerPercentage = distanceFromStartOfRuleLayer / layerSize;
-                // Apply modifier to allowance
-                allowance = p.AllowanceModifier(allowance, distanceFromStartOfRuleLayerPercentage);
-            }
+            // if (p.CustomRangeCheck != null)
+            // {
+            //     var layerSize = endDistance - startDistance;
+            //     var distanceFromStartOfRuleLayer = distancePerc - startDistance;
+            //     var distanceFromStartOfRuleLayerPercentage = distanceFromStartOfRuleLayer / layerSize;
+            //     // Apply modifier to allowance
+            //     allowance = p.AllowanceModifier(allowance, distanceFromStartOfRuleLayerPercentage);
+            // }
+            if (p.Noise1Range is var (start1, end1) && (noise1Value < start1 || noise1Value > end1)) return false;
+            if (p.Noise2Range is var (start2, end2) && (noise2Value < start2 || noise2Value > end2)) return false;
 
-            var totalDistanceFromTargetAcrossAllNoiseLayers = 0f;
-
-
-            if (p.Noise1Target.HasValue)
-                totalDistanceFromTargetAcrossAllNoiseLayers += Math.Abs(noise1Value - p.Noise1Target.Value);
-
-            if (p.Noise2Target.HasValue)
-                totalDistanceFromTargetAcrossAllNoiseLayers += Math.Abs(noise2Value - p.Noise2Target.Value);
-
-            return totalDistanceFromTargetAcrossAllNoiseLayers <= allowance;
+            return true;
         }
     }
 }
