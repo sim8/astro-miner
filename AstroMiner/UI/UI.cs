1e2558c6a9da9cf6b79b232045b87b56fb4c52c3
Simeon Lees <simeon.k.lees@gmail.com>
2025-08-17 11:17:18 +0100

    move uistate

--- AstroMiner/UI/UI.cs
+++ AstroMiner/UI/UI.cs
@@ -1,71 +1,7 @@
 using AstroMiner.Definitions;
-using AstroMiner.Effects;
 using Microsoft.Xna.Framework;
 
 namespace AstroMiner.UI;
-
-public class UIState(BaseGame game)
-{
-    public bool IsInMainMenu { get; set; } = true;
-    public bool IsInPauseMenu { get; set; }
-    public bool IsDebugMenuOpen { get; set; } = false;
-    public bool IsInventoryOpen { get; set; }
-    public bool IsLaunchConsoleOpen { get; set; }
-    public bool IsInShopMenu { get; set; }
-    public int sellConfirmationItemIndex { get; set; } = -1;
-
-    // TODO all very temporary. Need a proper way of tracking dialog
-    public bool IsInDialog { get; set; } = false;
-    public int DialogIndex { get; set; } = 0;
-
-    public ScrollingEffectManager StarBackground { get; init; } = new();
-
-    public void Update(GameTime gameTime, ActiveControls activeControls)
-    {
-        if (IsInMainMenu)
-        {
-            if (StarBackground.Layers.Count == 0)
-            {
-                StarBackground.AddLayer(new ScrollingEffectLayer
-                {
-                    TextureName = "star",
-                    TextureSize = 170,
-                    Speed = 8f,
-                    Density = 9f,
-                    MinOpacity = 0.1f,
-                    MaxOpacity = 0.2f
-                });
-                StarBackground.AddLayer(new ScrollingEffectLayer
-                {
-                    TextureName = "star",
-                    TextureSize = 170,
-                    Speed = 16f,
-                    Density = 7f,
-                    MinOpacity = 0.4f,
-                    MaxOpacity = 0.6f
-                });
-                // StarBackground.AddLayer(new ScrollingEffectLayer
-                // {
-                //     TextureName = "star",
-                //     TextureSize = 170,
-                //     Speed = 30f,
-                //     Density = 2f,
-                //     MinOpacity = 0.8f,
-                //     MaxOpacity = 1.0f
-                // });
-            }
-            else
-            {
-                StarBackground.Update(gameTime, game.Graphics.GraphicsDevice.Viewport.Width,
-                    game.Graphics.GraphicsDevice.Viewport.Height);
-            }
-        }
-        else if (StarBackground.Layers.Count > 0)
-        {
-            StarBackground.Layers.Clear();
-        }
-    }
-}
 
 public class UI(BaseGame game)
 {
