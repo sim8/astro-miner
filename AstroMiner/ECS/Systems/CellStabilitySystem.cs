a281dfcbe2a9399fc852cb50796ddc3f0a01ff51
Simeon Lees <simeon.k.lees@gmail.com>
2025-09-07 10:50:44 +0100

    tidy up

--- AstroMiner/ECS/Systems/CellStabilitySystem.cs
+++ AstroMiner/ECS/Systems/CellStabilitySystem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using AstroMiner.AsteroidWorld;
 using AstroMiner.Definitions;
 using AstroMiner.ECS.Components;
@@ -7,83 +8,73 @@ using Microsoft.Xna.Framework;
 
 namespace AstroMiner.ECS.Systems;
 
-public class CellStabilitySystem : System
+public class CellStabilitySystem(Ecs ecs, BaseGame game) : System(ecs, game)
 {
-    public const float MaxUpdateDistance = 2f;
-    public const float DamageMultiplier = 0.1f;
+    private const float MaxUpdateDistance = 2f;
+    private const float DamageMultiplier = 0.1f;
 
     public const float CriticalStabilityThreshold = 0.2f; // Could be different thresholds for collapsing/explosive
-
-
-    public CellStabilitySystem(Ecs ecs, BaseGame game) : base(ecs, game)
-    {
-    }
 
 
     public override void Update(GameTime gameTime, ActiveControls activeControls)
     {
         if (game.Model.ActiveWorld != World.Asteroid) return;
 
-        foreach (var (x, y) in game.Model.Asteroid.CriticalStabilityCells)
+        foreach (var (x, y) in game.Model.Asteroid.CriticalStabilityCells.ToList())
         {
-            var newStability = UpdateCellStability(x, y, (stability) => stability - gameTime.ElapsedGameTime.Milliseconds / GameConfig.CollapsingFloorSpreadTime);
-            if (newStability == 0)
-            {
-                game.Model.Asteroid.CriticalStabilityCells.Remove((x, y));
-            }
+            var newStability = UpdateCellStability(x, y,
+                stability =>
+                    stability - gameTime.ElapsedGameTime.Milliseconds / (float)GameConfig.CollapsingFloorSpreadTime);
+            if (newStability == 0) game.Model.Asteroid.CriticalStabilityCells.Remove((x, y));
         }
-
-        // TODO - modify above to have same burndown as collapsing
-        // TODO - add trigger neighbours logic (-= .1?)
-        // TODO - update animation to read percentages
-        // TODO - move onto explosives
-
     }
 
     public float UpdateCellStability(int x, int y, Func<float, float> stabilityFunc)
     {
         var cellState = game.StateManager.AsteroidWorld.Grid.GetCellState(x, y);
         var prevStability = cellState.Stability;
+
+
+        if (prevStability == 1)
+            // Temporary? Only really relevant to collapsing cells
+            return prevStability;
+
+
         var newStability = stabilityFunc(cellState.Stability);
         cellState.Stability = Math.Max(0, newStability);
 
-        if (prevStability == cellState.Stability)
-        {
-            return cellState.Stability;
-        }
+        if (prevStability == cellState.Stability) return cellState.Stability;
 
-        if (cellState.Stability < CriticalStabilityThreshold && !game.Model.Asteroid.CriticalStabilityCells.Contains((x, y)))
+        if (cellState.Stability < CriticalStabilityThreshold &&
+            !game.Model.Asteroid.CriticalStabilityCells.Contains((x, y)))
         {
             // TODO do we still need FloorType.CollapsingLavaCracks?
-            if (cellState.FloorType == FloorType.LavaCracks)
-            {
-                cellState.FloorType = FloorType.CollapsingLavaCracks;
-            }
+            if (cellState.FloorType == FloorType.LavaCracks) cellState.FloorType = FloorType.CollapsingLavaCracks;
             game.Model.Asteroid.CriticalStabilityCells.Add((x, y));
         }
+
         if (cellState.Stability == 0)
-        {
             GridState.Map4Neighbors(x, y,
-                (nx, ny) => { UpdateCellStability(nx, ny, (stability) => stability - 0.1f); });
-        }
+                (nx, ny) => { UpdateCellStability(nx, ny, stability => stability - 0.1f); });
 
         return cellState.Stability;
     }
 
     public void UpdateCellStabilityForMovement(PositionComponent position, float distanceMoved)
     {
-
         if (game.Model.ActiveWorld != World.Asteroid) return;
 
         if (!game.StateManager.AsteroidWorld.IsInMiner) return;
 
 
-        var cellsInRadius = AsteroidGridHelpers.GetCellsInRadius(position.CenterPosition.X, position.CenterPosition.Y, MaxUpdateDistance);
+        var cellsInRadius =
+            AsteroidGridHelpers.GetCellsInRadius(position.CenterPosition.X, position.CenterPosition.Y,
+                MaxUpdateDistance);
         foreach (var (x, y, cellDistance) in cellsInRadius)
         {
             var percentageOfDamageToApply = cellDistance / MaxUpdateDistance;
             var damageToApply = percentageOfDamageToApply * distanceMoved * DamageMultiplier;
-            UpdateCellStability(x, y, (stability) => stability - damageToApply);
+            UpdateCellStability(x, y, stability => stability - damageToApply);
         }
     }
 }
