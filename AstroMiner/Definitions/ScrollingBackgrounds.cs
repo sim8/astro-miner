a92bc88bca9f5e1afa7096455e7b5abfa4fc676a
Simeon Lees <simeon.k.lees@gmail.com>
2025-09-08 23:27:42 +0100

    get rid of cloud utils

--- AstroMiner/Definitions/ScrollingBackgrounds.cs
+++ AstroMiner/Definitions/ScrollingBackgrounds.cs
@@ -2,18 +2,6 @@ using System.Collections.Generic;
 
 namespace AstroMiner.Definitions;
 
-public record CloudConfig
-{
-    public int CloudTextureSizePx { get; init; } = 128;
-    public float CloudTextureScale { get; init; } = 1.0f;
-    public float CloudsPerGridCell { get; init; } = 0.1f;
-    public int CloudSeed { get; init; } = 42;
-    public float CloudSpeed { get; init; } = 1f;
-    public float CloudParallaxFactorX { get; init; } = 0.6f;
-    public float CloudParallaxFactorY { get; init; } = 0.3f;
-    public string TextureName { get; init; } = "cloud-background";
-}
-
 public record ScrollingBackgroundConfig
 {
     // Land configuration
@@ -23,10 +11,7 @@ public record ScrollingBackgroundConfig
     public float LandSpeed { get; init; } = 0.5f;
     public float LandParallaxFactorX { get; init; } = 0.4f;
     public float LandParallaxFactorY { get; init; } = 0.2f;
-    public string LandTextureName { get; init; } = "mountains-nice-tiled";
-
-    // Cloud configuration
-    public List<CloudConfig> CloudLayers { get; init; } = new();
+    public string LandTextureName { get; init; } = Tx.Backgrounds.Oizus;
 }
 
 public static class ScrollingBackgrounds
@@ -40,44 +25,17 @@ public static class ScrollingBackgrounds
         LandParallaxFactorX = 0.4f,
         LandParallaxFactorY = 0.2f,
         LandTextureName = Tx.Backgrounds.Oizus,
-        CloudLayers =
-        [
-            // new CloudConfig
-            // {
-            //     CloudTextureScale = 0.5f,
-            //     CloudsPerGridCell = 0.1f,
-            //     CloudSeed = 42,
-            //     CloudSpeed = 1f,
-            //     CloudParallaxFactorX = 0.6f,
-            //     CloudParallaxFactorY = 0.3f
-            // },
-            // // Foreground cloud layer
-
-            // new CloudConfig
-            // {
-            //     CloudTextureScale = 1f,
-            //     CloudsPerGridCell = 0.05f,
-            //     CloudSeed = 123,
-            //     CloudSpeed = 1.5f,
-            //     CloudParallaxFactorX = 0.8f,
-            //     CloudParallaxFactorY = 0.4f
-            // }
-        ]
     };
 
     private static readonly ScrollingBackgroundConfig OizusOrbit = new()
     {
-        LandTextureWidthPx = 1600,
-        LandTextureHeightPx = 1600,
-        LandTextureScale = 0.0625f,
-        LandSpeed = 0.2f,
+        LandTextureWidthPx = 640,
+        LandTextureHeightPx = 640,
+        LandTextureScale = 1f,
+        LandSpeed = 5f,
         LandParallaxFactorX = 0.4f,
         LandParallaxFactorY = 0.2f,
         LandTextureName = Tx.Backgrounds.Oizus,
-        CloudLayers =
-        [
-
-        ]
     };
 
     public static ScrollingBackgroundConfig? GetScrollingBackgroundConfig(World activeWorld)
